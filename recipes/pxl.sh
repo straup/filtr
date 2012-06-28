c8226bee0c99b090794e7e848d82b2389c30fd6b
straup <straup@gmail.com>
2012-06-28 22:02:46 +0200

    always specify -f JPEG when calling pxl.sh (for now)

--- recipes/pxl.sh
+++ recipes/pxl.sh
@@ -2,6 +2,8 @@
 
 echo "[${FILTER}] triangulize ${INPUT} (${OUTPUT})"
 
+# to do: check that INPUT is a jpeg before setting -f
 # to do: adjust -t based on the size of the photo...
-${PYTHON} ${UTILS}/triangulizor.py -t 24 ${INPUT} ${OUTPUT}
+
+${PYTHON} ${UTILS}/triangulizor.py -t 24 -f JPEG ${INPUT} ${OUTPUT}
 
